98339861824950dec5cde2a9dc399672f864bf1e
Robson Fernando Trasel de Souza <robson.trasel@gmail.com>
2023-01-17 23:34:14 -0300

    chore: added email roouter

--- src/routes/api.routes.ts
+++ src/routes/api.routes.ts
@@ -1,15 +1,14 @@
 import express from 'express'
 import commentsController from '../controllers/comments/comments.controller'
-
+import emailController from '../controllers/emails/email.controller'
 
 const router = express.Router()
-
 
 router.get('/comments', commentsController.getComments)
 router.post('/comments', commentsController.createComments)
 router.put('/comments/approve/:id', commentsController.setApproved)
 router.put('/comments/reject/:id', commentsController.setRejected)
-
+router.post('/send-mail', emailController.enviarEmails)
 
 
 export default router
