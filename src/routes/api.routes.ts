45c4e63dbbd77d2e28f5ec7717c867640c9991d9
Robson Fernando Trasel de Souza <robson.trasel@gmail.com>
2023-01-17 21:03:50 -0300

    refactor: moved the logic to a dedicated file

--- src/routes/api.routes.ts
+++ src/routes/api.routes.ts
@@ -1,65 +1,15 @@
-import express, {Request, Response} from 'express'
-import client  from '../data/connections/database.config'
+import express from 'express'
+import commentsController from '../controllers/comments/comments.controller'
+
 
 const router = express.Router()
 
 
-router.get('/comments', async (req: Request, res: Response) => {
-    const limit = Number(req.query.limit) || 10;
-    const offset = Number(req.query.offset) || 0;
-    try {
-        const result = await client.query(`SELECT * FROM comments WHERE status = 'aprovado' LIMIT ${limit} OFFSET ${offset}`);
-        res.status(200).json(result.rows);
-      } catch (err) {
-        console.log(err);
-        res.status(500);
-      }
-})
+router.get('/comments', commentsController.getComments)
+router.post('/comments', commentsController.createComments)
+router.put('/comments/approve/:id', commentsController.setApproved)
+router.put('/comments/reject/:id', commentsController.setRejected)
 
-
-router.post('/comments', async (req: Request, res: Response) => {
-    try {
-        const {
-            name, comment
-        } = req.body
-
-        if (!name) {
-            return res.status(400).json({ error: 'Nome é obrigatorio' });
-        } else if(!comment) {
-            return res.status(400).json({ error: 'Comentario é obrigatorios' });
-
-        }
-        const sql = 'INSERT INTO comments (name, comment) VALUES ($1, $2, $3)'
-        const values = [
-            name, comment
-        ]
-        await client.query(sql, values)
-        res.status(201).send({
-            message: "Comentario criado com sucesso!"
-        })
-    } catch (error) {
-        res.status(500).send({
-            message: "Erro ao criar comentario!"
-        })
-    }
-})
-
-router.put('/comments/approve/:id', async (req: Request, res: Response) => {
-    const commentId = req.params.id
-    try {
-        const result = await client.query(`SELECT status FROM comments WHERE id = ${commentId}`)
-
-        if(result.rows[0].status === 'pendente') {
-            await client.query(`UPDATE comments SET status = 'aprovado' WHERE id = ${commentId}`);
-            res.status(200).send('Comentario foi aprovado com sucesso!');
-        } else {
-            res.status(400).send('Comentario ja foi aprovado!')
-        }       
-      } catch (err) {
-        console.log(err);
-        res.status(500).send();
-    }
-})
 
 
 export default router
