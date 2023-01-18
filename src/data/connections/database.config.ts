9ad18e0020742d5c11787a4913e6aa6bccb6f37e
Robson Fernando Trasel de Souza <robson.trasel@gmail.com>
2023-01-17 21:09:17 -0300

    chore: changed the importer for db string connect

--- src/data/connections/database.config.ts
+++ src/data/connections/database.config.ts
@@ -2,8 +2,8 @@ import * as pg from 'pg'
 import dotenv from 'dotenv'
 dotenv.config()
 
-const connecString = "postgres://dwrsjgut:9iacSXFQWwzNDTR-RxFUCN_03xZPiNS6@babar.db.elephantsql.com/dwrsjgut"
-const client = new pg.Client(connecString)
+const connectString = process.env.DB_URL
+const client = new pg.Client(connectString)
 
 client.connect((err) => {
     if(err) {
