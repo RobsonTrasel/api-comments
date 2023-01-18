a047d8da9a9238141564e29dd35f5c94dcb0e3ac
Robson Fernando Trasel de Souza <robson.trasel@gmail.com>
2023-01-17 23:40:14 -0300

    refactor: refactored the commentcontroller  #1

--- src/controllers/comments/comments.controller.ts
+++ src/controllers/comments/comments.controller.ts
@@ -1,84 +1,66 @@
 import { Request, Response } from "express";
-import client from '../../data/connections/database.config'
+import client from "../../data/connections/database.config";
 
 class CommentsController {
-    
-    async getComments(req: Request, res: Response) {
-        const limit = Number(req.query.limit) || 10;
-        const offset = Number(req.query.offset) || 0;
-        try {
-            const result = await client.query(`SELECT * FROM comments WHERE status = 'aprovado' LIMIT ${limit} OFFSET ${offset}`);
-            res.status(200).json(result.rows);
-        } catch(err) {
-            console.log(err)
-            res.status(500)
-        }
+  async getComments(req: Request, res: Response) {
+    const limit = Number(req.query.limit) || 10;
+    const offset = Number(req.query.offset) || 0;
+    const result = await client.query(
+      `SELECT * FROM comments WHERE status = 'aprovado' LIMIT ${limit} OFFSET ${offset}`
+    );
+    res.status(200).json(result.rows);
+  }
+
+  async createComments(req: Request, res: Response) {
+    const { name, comment } = req.body;
+
+    console.log(name, comment);
+
+    if (!name) {
+      return res.status(400).json({ error: "Nome é obrigatorio" });
+    } else if (!comment) {
+      return res.status(400).json({ error: "Comentario é obrigatorios" });
     }
 
-    async createComments(req: Request, res: Response) {
-        try {
-            const {
-                name, comment
-            } = req.body
+    const sql = "INSERT INTO comments (name, comment) VALUES ($1, $2)";
+    const values = [name, comment];
+    await client.query(sql, values);
+    res.status(201).send({
+      message: "Comentario criado com sucesso!",
+    });
+  }
 
-            console.log(name, comment)
+  async setApproved(req: Request, res: Response) {
+    const commentId = req.params.id;
 
-            if (!name) {
-                return res.status(400).json({ error: 'Nome é obrigatorio' });
-            } else if(!comment) {
-                return res.status(400).json({ error: 'Comentario é obrigatorios' });
-            }
+    const result = await client.query(
+      `SELECT status FROM comments WHERE id = ${commentId}`
+    );
 
-            const sql = 'INSERT INTO comments (name, comment) VALUES ($1, $2)'
-            const values = [
-                name, comment
-            ]
-            await client.query(sql, values)
-            res.status(201).send({
-                message: "Comentario criado com sucesso!"
-            })
-        } catch (err) {
-            console.log(err)
-            res.status(500).send({
-                message: 'Erro ao criar comentario'
-            })
-        }
+    if (result.rows[0].status === "pendente") {
+      await client.query(
+        `UPDATE comments SET status = 'aprovado' WHERE id = ${commentId}`
+      );
+      res.status(200).send("Comentario foi aprovado com sucesso!");
+    } else {
+      res.status(400).send("Comentario ja foi aprovado ou rejeitado");
     }
+  }
 
-    async setApproved(req: Request, res: Response) {
-        const commentId = req.params.id
-        try {
-            const result = await client.query(`SELECT status FROM comments WHERE id = ${commentId}`)
-
-            if(result.rows[0].status === 'pendente') {
-                await client.query(`UPDATE comments SET status = 'aprovado' WHERE id = ${commentId}`);
-                res.status(200).send('Comentario foi aprovado com sucesso!');
-            } else {
-                res.status(400).send('Comentario ja foi aprovado ou rejeitado')
-            }       
-        } catch (err) {
-            console.log(err);
-            res.status(500).send();
-        }
+  async setRejected(req: Request, res: Response) {
+    const commentId = req.params.id;
+    const result = await client.query(
+      `SELECT status FROM comments WHERE id = ${commentId}`
+    );
+    if (result.rows[0].status === "pendente") {
+      await client.query(
+        `UPDATE comments SET status = 'rejected' WHERE id = ${commentId}`
+      );
+      res.status(200).send("Comentario foi rejeitado com sucesso");
+    } else {
+      res.status(400).send("Comentario ja foi aprovado ou rejeitado");
     }
-
-    async setRejected(req: Request, res: Response) {
-        const commentId = req.params.id;
-        try {
-            const result = await client.query(`SELECT status FROM comments WHERE id = ${commentId}`);
-            if (result.rows[0].status === 'pendente') {
-            await client.query(`UPDATE comments SET status = 'rejected' WHERE id = ${commentId}`);
-            res.status(200).send('Comentario foi rejeitado com sucesso');
-            } else {
-            res.status(400).send('Comentario ja foi aprovado ou rejeitado');
-            }
-        } catch (err) {
-            console.log(err);
-            res.sendStatus(500);
-        }
-    }
-
-
+  }
 }
 
-export default new CommentsController()
+export default new CommentsController();
