e80c8a47792af20d466830af039c27ad48c7cf3b
Robson Fernando Trasel de Souza <robson.trasel@gmail.com>
2023-01-25 22:37:56 -0300

    feat: created controller for email

--- src/controllers/emails/email.controller.ts
+++ src/controllers/emails/email.controller.ts
@@ -1,73 +1,90 @@
 import { Request, Response } from "express";
-import nodemailer from 'nodemailer'
+import nodemailer, { Transporter} from "nodemailer";
 import { check, validationResult } from "express-validator";
-import dotenv from 'dotenv'
+import { google } from "googleapis";
+import dotenv from "dotenv";
 import SMTPTransport from "nodemailer/lib/smtp-transport";
-dotenv.config()
+dotenv.config();
 
-interface EmailType {
+const OAuth2 = google.auth.OAuth2
 
+interface EmailTypes {
+    host: string;
+    port: number;
+    secure: boolean;
+    auth: { user: string; pass: string; }
 }
 
 
 class EmailController {
-    async enviarEmails(req: Request, res: Response) {
-        check('name').not().isEmpty()
-        check('subject').not().isEmpty()
-        check('description').not().isEmpty()
-        const errors = validationResult(req)
+  async enviarEmails(req: Request, res: Response) {
 
-        if(!errors.isEmpty()) {
-            return res.status(422).json({
-                errors: errors.array()
-            })
-        }
+    const oauth2client = new OAuth2(
+        process.env.MAIL_CLIENT_ID,
+        process.env.MAIL_CLIENT_SECRET,
+        "https://developers.google.com/oauthplayground"
+    )
 
-        const {
-            name, 
-            subject,
-            description
-        } = req.body
+    oauth2client.setCredentials({
+        refresh_token: process.env.MAIL_REFRESH_TOKEN
+    })
 
-        const sender = nodemailer.createTransport({
-            host: process.env.MAIL_HOST,
-            port: process.env.MAIL_PORT,
-            secure: true,
-            auth: {
-                type: 'oauth2',
-                user: process.env.MAIL_USER,
-                pass: process.env.MAIL_PASSWORD,
-                clientId: process.env.MAIL_CLIENT_ID,
-                clientSecret: process.env.MAIL_CLIENT_SECRET,
-                accessToken: process.env.MAIL_ACCESS_TOKEN,
-                expires: 3600
-            }
-        } as SMTPTransport.Options)
+    const accessToken = await new Promise((resolve, reject) => {
+        oauth2client.getAccessToken((err, token) => {
+          if (err) {
+            reject("Failed to create access token :(");
+          }
+          resolve(token);
+        });
+    });
 
-        const mailOptions = {
-            from: process.env.MAIL_USER,
-            to: 'robson.trasel@gmail.com',
-            subject,
-            text: description,
-            html: `
+    check("name").not().isEmpty();
+    check("subject").not().isEmpty();
+    check("description").not().isEmpty();
+
+    const errors = validationResult(req);
+
+    if (!errors.isEmpty()) {
+      return res.status(422).json({
+        errors: errors.array(),
+      });
+    }
+
+    const { name, subject, description } = req.body;
+
+    const sender = nodemailer.createTransport({
+      host: process.env.MAIL_HOST,
+      port: 587,
+      secure: false,
+      auth: {
+        type: 'OAuth2',
+        accessToken: accessToken,
+        clientId: process.env.MAIL_CLIENT_ID,
+        clientSecret: process.env.MAIL_CLIENT_SECRET,
+        user: process.env.MAIL_USER,
+        pass: process.env.MAIL_PASSWORD,
+      },
+      tls: {
+        rejectUnauthorized: false
+      }
+    } as SMTPTransport.Options);
+
+    const mailOptions = {
+      from: process.env.MAIL_USER,
+      to: "robson.trasel@gmail.com",
+      subject,
+      text: description,
+      html: `
             <p> Nome: ${name} </p>
             <p> Assunto: ${subject} </p>
             <p> Descrição: ${description} </p>
-            `
-        }
-
-        try {
-            await sender.sendMail(mailOptions)
-            res.status(200).json({
-                message: 'Email enviado'
-            })
-        } catch(err) {
-            console.error(err)
-            res.status(500).json({
-                message: 'Erro ao enviar email'
-            })
-        }
-    }
+            `,
+    };
+    await sender.sendMail(mailOptions);
+    res.status(200).json({
+      message: "Email enviado",
+    });
+  }
 }
 
-export default new EmailController
+export default new EmailController();
