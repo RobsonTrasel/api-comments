2ade6a1056291fed40a33106b06924e5fd8f78a7
Robson Fernando Trasel de Souza <robson.trasel@gmail.com>
2023-01-25 22:40:25 -0300

    chore: added middleware and new route login

--- src/routes/api.routes.ts
+++ src/routes/api.routes.ts
@@ -1,13 +1,16 @@
 import express from 'express'
 import commentsController from '../controllers/comments/comments.controller'
 import emailController from '../controllers/emails/email.controller'
+import { convertIdToNumber } from '../middlewares/convertIdToNumber';
+import AuthController from '../controllers/auth/auth.controller';
 
 const router = express.Router()
 
 router.get('/comments', commentsController.getComments)
 router.post('/comments', commentsController.createComments)
-router.put('/comments/approve/:id', commentsController.setApproved)
-router.put('/comments/reject/:id', commentsController.setRejected)
+router.put('/comments/approve/:id', convertIdToNumber ,commentsController.setApproved)
+router.put('/comments/reject/:id', convertIdToNumber ,commentsController.setRejected)
+router.post('/login', AuthController.login)
 router.post('/send-mail', emailController.enviarEmails)
 
 
