725fd3abab131d10ee283fbdd1daaa504178158b
RobsonTrasel <robson.trasel@gmail.com>
2023-01-26 14:11:59 -0300

    docs: created doc to auth methods controller

--- src/controllers/auth/auth.controller.ts
+++ src/controllers/auth/auth.controller.ts
@@ -3,6 +3,13 @@ import { User } from "../../models/user";
 import { generateToken } from '../../helper/generateToken';
 
 class AuthController {
+    /**
+     * @function login - Auth the user in dashboard admin
+     * @async
+     * @param {Request} req - Express request object
+     * @param {Response} res - Express response object
+     * @description This function handles the login process for a user. It takes in the email and password from the request body, and attempts to find a user with those credentials. If a user is found, it generates a JSON Web Token (JWT) with the user's ID and sends it back as the response. If no user is found, it sends a 401 Unauthorized response with an error message.
+     */
     async login(req: Request, res: Response) {
         const { email, password } = req.body
         const user = await User.findByEmailAndPassword(email, password)
